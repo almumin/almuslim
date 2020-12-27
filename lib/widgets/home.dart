bb21d5b2dd684fe71913f7aed0ebae1d369eb9f0
Forhad <shifat2sadi@yahoo.com>
2020-12-28 01:48:56 +0200

    updated ui

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hijri/hijri_calendar.dart';
 
 class HomeView extends StatelessWidget {
+  HijriCalendar _hijriToday = new HijriCalendar.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Al-Muslim"),
-        backgroundColor: Colors.blueGrey.shade900,
+        title: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  "Sunday, 27 December 2020",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black, fontSize: 12),
+                ),
+                Text(
+                  "${_hijriToday.hDay} ${_hijriToday.longMonthName} ${_hijriToday.hYear}",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black, fontSize: 12),
+                ),
+              ],
+            ),
+          ),
+        ),
+        backgroundColor: Colors.white,
       ),
       backgroundColor: Colors.grey.shade100,
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(14.0),
-            child: Column(
-              children: [
-                Text("Sunday, 27 December 2020"),
-                Text("Sunday, 27 December 2020"),
-              ],
-            ),
-          ),
           Container(
             child: Row(
               children: [
