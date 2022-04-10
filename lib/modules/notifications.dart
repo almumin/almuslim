9ba9b13149a28dee84c442c8786fef98050200b3
Al-Muslim <shifat2sadi@yahoo.com>
2022-04-10 04:57:52 +0300

    upgrade flutter version and solve other dependencies
    - upgrade to new flutter version
    - add sample reminders.dart file using localnotification

--- lib/modules/notifications.dart
+++ lib/modules/notifications.dart
@@ -33,6 +33,33 @@ class NotificationService {
     await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   }
 
+  Future<void> showScheduleAlertNotification(int id, String title, String body, int seconds) async {
+    var time = Time(8, 0, 0);
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.now(tz.local).add(Duration(seconds: seconds)),
+      const NotificationDetails(
+        android: AndroidNotificationDetails(
+            'main_channel',
+            'Main Channel',
+            importance: Importance.max,
+            priority: Priority.max,
+            icon: '@drawable/ic_flutternotification'
+        ),
+        iOS: IOSNotificationDetails(
+          sound: 'default.wav',
+          presentAlert: true,
+          presentBadge: true,
+          presentSound: true,
+        ),
+      ),
+      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+    );
+  }
+
   Future<void> showNotification(int id, String title, String body, int seconds) async {
     await flutterLocalNotificationsPlugin.zonedSchedule(
       id,
